79f70ade8bb96379c672b96af61a15d740668aaf
Bianca Rhodes <brhodes@nvidia.com>
2019-04-08 09:35:10 -0400

    Removed dependency on hdfs

--- tutorials/cyber/raw_data_generator/run_raw_data_generator.py
+++ tutorials/cyber/raw_data_generator/run_raw_data_generator.py
@@ -1,10 +1,7 @@
 import argparse
-import configparser
 from raw_data_generator import raw_data_generator
-from hdfs3 import HDFileSystem
 
-def main(input_file, out_dir, out_format, hdfs_config):
-    hdfs_conn = HDFileSystem(host = hdfs_config['host'], port=int(hdfs_config['port']), user=hdfs_config['user'])
+def main(input_file, out_dir, out_format):
     rdg = raw_data_generator.RawDataGenerator(hdfs_conn)
     rdg.generate_raw_data(input_file, out_dir, out_format)
 
@@ -15,6 +12,4 @@ if __name__ == '__main__':
     PARSER.add_argument('output_directory', help='Full path to hdfs desired output directory')
     PARSER.add_argument('--format', choices=['csv', 'json'], default='csv', type=str)
     args = PARSER.parse_args()
-    config = configparser.ConfigParser()
-    config.read('run_raw_data_generator.ini')
-    main(args.input_file, args.output_directory, args.format, config['HDFS'])
+    main(args.input_file, args.output_directory, args.format)
