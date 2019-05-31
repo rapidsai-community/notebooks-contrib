164836ad82fb6cefe2f7129ece72cc28ead12ab6
Randy Gelhausen <rgelhau@gmail.com>
2019-05-31 09:42:16 -0400

    adding colab-setup script

---
+++ utils/colab-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ ! -f Miniconda3-4.5.4-Linux-x86_64.sh ]; then
+    echo "Removing existing dask & xgboost packages"
+    # remove existing xgboost and dask installs
+    pip uninstall -y xgboost dask distributed
+
+    # intall miniconda
+    echo "Installing conda"
+    wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh
+    chmod +x Miniconda3-4.5.4-Linux-x86_64.sh
+    bash ./Miniconda3-4.5.4-Linux-x86_64.sh -b -f -p /usr/local
+
+    echo "Installing RAPIDS packages"
+    # install RAPIDS packages
+    conda install -y --prefix /usr/local \
+      -c rapidsai-nightly/label/xgboost -c rapidsai-nightly -c nvidia -c conda-forge \
+      python=3.6 cudatoolkit=10.0 \
+      cudf dask-cudf \
+      cuml dask-cuml \
+      cugraph \
+      xgboost=>0.83 dask-xgboost=>0.2 \
+      gcsfs
+    
+    echo "Copying shared object files to /usr/lib"
+    # copy .so files to /usr/lib, where Colab's Python looks for libs
+    cp /usr/local/lib/libcudf.so /usr/lib/libcudf.so
+    cp /usr/local/lib/librmm.so /usr/lib/librmm.so
+    cp /usr/local/lib/libxgboost.so /usr/lib/libxgboost.so
+    cp /usr/local/lib/libnccl.so /usr/lib/libnccl.so
+
+    echo "Installing ngrok"
+    # expose Dask's status dashboard to a public URL
+    wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+    unzip ngrok-stable-linux-amd64.zip
+    mv ngrok /usr/local/bin/ngrok
+fi
