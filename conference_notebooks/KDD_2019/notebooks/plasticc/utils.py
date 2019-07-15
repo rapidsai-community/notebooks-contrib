f2728546c73480c30a371437138f379c90508542
Corey J. Nolet <cjnolet@gmail.com>
2019-07-15 18:33:13 -0400

    Tutorial steps largely done.

--- conference_notebooks/KDD_2019/notebooks/plasticc/utils.py
+++ conference_notebooks/KDD_2019/notebooks/plasticc/utils.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def scatter(x,y,values,xlabel='x',ylabel='y',title=None):
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+def scatter(x,y,values,xlabel='x',ylabel='y',title=None,xlim=None):
     """
     Builds a scatter plot specific to the LSST data format by plotting 
     the flux over time for different passbands for a single object.
@@ -20,8 +23,11 @@ def scatter(x,y,values,xlabel='x',ylabel='y',title=None):
     plt.legend(ps,bs,scatterpoints=1)
     if title is not None:
         plt.title(title)
-    
-    plt.xlim([np.min(x)-10,np.min(x)+1500])
+    plt.grid()
+    if xlim is None:
+        plt.xlim([np.min(x)-10,np.min(x)+1500])
+    else:
+        plt.xlim(xlim)
     plt.ylabel('y: %s'%ylabel)
     plt.xlabel('x: %s'%xlabel)
     
@@ -41,3 +47,57 @@ def groupby_aggs(df,aggs,col = "object_id"):
             res = tmp if res is None else  res.merge(tmp,on=[col],how='left')
         df.drop_column(i)
     return res
+
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots(figsize=(28, 8))
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
