c5394b9d5a8f5a709b487e7194f612d86dc1b805
afender <afender@nvidia.com>
2019-08-07 19:07:51 -0500

    Updated content

--- intermediate_notebooks/examples/cugraph/get_data.sh
+++ intermediate_notebooks/examples/cugraph/get_data.sh
@@ -4,5 +4,5 @@ echo Downloading ...
 wget https://s3.us-east-2.amazonaws.com/rapidsai-data/cugraph/benchmark/twitter.csv.gz
 
 echo Decompressing ...
-gunzip twitter.csv.gz  
+gunzip twitter.csv.gz 
 
