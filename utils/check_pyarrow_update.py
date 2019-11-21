796071f01ce34dc8ad3da70b0ca06b4dca82f297
Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>
2019-11-20 18:30:59 -0800

    Update check_pyarrow_update.py
    working through issues

--- utils/check_pyarrow_update.py
+++ utils/check_pyarrow_update.py
@@ -7,19 +7,17 @@ print('Let us check on that pyarrow version...')
 print('***********************************************************************')
 print()
 
-if ('pyarrow' in sys.modules.keys()):
-  print('pyarrow exists')
+pyarrow_version = sys.modules['pyarrow'].__version__
+f = re.search("0.15.+", pyarrow_version)
+if(f == None):
+  for key in list(sys.modules.keys()):
+    if key.startswith('pyarrow'):
+      del sys.modules[key]
+      print(f"unloaded pyarrow {pyarrow_version}")
+  import pyarrow
   pyarrow_version = sys.modules['pyarrow'].__version__
-  f = re.search("0.15.+", pyarrow_version)
-  if(f == None):
-    for key in list(sys.modules.keys()):
-      if key.startswith('pyarrow'):
-        del sys.modules[key]
-        print(f"unloaded pyarrow {pyarrow_version}")
-    import pyarrow
-    pyarrow_version = sys.modules['pyarrow'].__version__
-    print(f"loaded pyarrow {pyarrow_version}")
-    del(pyarrow_version)
+  print(f"loaded pyarrow {pyarrow_version}")
+  del(pyarrow_version)
 
-  else: 
-    print(f"You're running pyarrow {pyarrow_version} and are good to go!")
+else: 
+  print(f"You're running pyarrow {pyarrow_version} and are good to go!")
